495d67397c83c7303415b69c688b597d4c0595c8
ashu-ram-g <jairam40101@gmail.com>
2025-01-16 21:27:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id =546431313
-api_hash = ""
-bot_token = "aaf4235465a4r3a1f3af13af1a4fa"
-auth_users = "46513214"
+api_id =18116881
+api_hash = "cca3bacf40fb3ebcb4f075b2e46ff1bd"
+bot_token = "7428927891:AAHoNzLYya8aRaGF2y-ngluSxzy29LlN3XI"
+auth_users = "1445673621"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "56416564654"
+osowner_users = "1445673621"
 owner_users = [int(num) for num in osowner_users.split(",")]
